bfab4803ac51e50ac013ae4b686cd77fc04cf6ba
Nul-led <jnaltz@gmail.com>
2024-05-15 14:29:47 +0200

    feat: give info header animation

--- web/src/components/InfoHeader.ts
+++ web/src/components/InfoHeader.ts
@@ -1,6 +1,7 @@
 import { Component } from "../core/Component";
 import Renderable from "../core/Renderable";
 import { ScreenAnchorX, ScreenAnchorY } from "../core/Viewport";
+import Animation, { AnimationType } from "../util/Animation";
 import Text from "../widgets/Text";
 
 export default class InfoHeader extends Component {
@@ -10,13 +11,19 @@ export default class InfoHeader extends Component {
         public y: number = 0,
         public anchorX: ScreenAnchorX = ScreenAnchorX.Center,
         public anchorY: ScreenAnchorY = ScreenAnchorY.Center,
+        public animation: Animation = new Animation(AnimationType.EaseOutExpo, 0.0075),
     ) {
         super();
     }
 
+    public resetAnimation() {
+        this.animation.timer = 0;
+    }
+
     public render(ctx: Renderable): void {
         const x = this.x.anchoredScreenSpace(this.anchorX);
-        const y = this.y.anchoredScreenSpace(this.anchorY);
+        this.animation.stepForward();
+        const y = this.y.anchoredScreenSpace(this.anchorY) * this.animation.latest;
 
         this.header.renderCentered(ctx, x, y);
     }
