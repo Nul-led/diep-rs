0b0e05e19ce732be918807db6b7830ed85081743
Nul-led <jnaltz@gmail.com>
2024-05-25 19:16:14 +0200

    feat: Invite component

---
+++ web/src/components/Invite.ts
@@ -0,0 +1,26 @@
+import { Component } from "../core/Component";
+import Renderable from "../core/Renderable";
+import { ScreenAnchorX, ScreenAnchorY } from "../core/Viewport";
+import Color from "../util/Color";
+import Text from "../widgets/Text";
+import { TextButton } from "../widgets/buttons/TextButton";
+
+export default class Invite extends Component {
+    public constructor(
+        public button: TextButton = new TextButton(new Text("Invite", 0.56 * 24), -135, 12, ScreenAnchorX.Max, ScreenAnchorY.Min, 120, 24, Color.fromRGB(119, 119, 119), Color.fromRGB(51, 51, 51)),
+        public x: number = -135,
+        public y: number = 12,
+        public anchorX: ScreenAnchorX = ScreenAnchorX.Max,
+        public anchorY: ScreenAnchorY = ScreenAnchorY.Min,
+    ) {
+        super();
+    }
+
+    public render(ctx: Renderable): void {
+        const x = this.x.anchoredScreenSpace(this.anchorX);
+        const y = this.y.anchoredScreenSpace(this.anchorY);
+
+        this.button.render(ctx, x, y);
+        this.button.onClick = () => {}; // TODO invite
+    }
+}
